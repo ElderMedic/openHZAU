6f7bcff8eb40796dfa890f69c62296d02722c084
ZonesTissyc <102893132+ZonesTissyc@users.noreply.github.com>
2025-06-30 14:19:51 +0800

    删除

--- itc-OnlineJudge刷题/itc-oj_3139_计算立方根.py
+++
@@ -1,9 +0,0 @@
-def calculate_output(a):
-    # 电话键盘数字对应的字母数量映射
-    keypad = {'0':0, '1':0, '2':3, '3':3, '4':3, 
-              '5':3, '6':3, '7':4, '8':3, '9':4}
-    total = sum(keypad[d] for d in str(a))
-    return total ** 3
-
-a = int(input())
-print(calculate_output(a))
